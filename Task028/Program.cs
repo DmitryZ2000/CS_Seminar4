a85dc96600da2b2b4d41f02ed19b82f9030fa5d7
DVZ <orionvega5@gmail.com>
2022-08-15 21:50:10 +0300

    Add Task028 и Task030

---
+++ Task028/Program.cs
@@ -0,0 +1,14 @@
+﻿double myfactoril(double a)
+{   
+       
+    double myfactoril = 1;
+    for (int i = 1; i <=a; i++)
+        {
+         myfactoril *=i;
+        }
+    
+    return myfactoril;
+}
+
+// Console.WriteLine("Введите произвольное число");
+Console.WriteLine(myfactoril(10));
