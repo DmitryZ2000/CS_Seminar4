aeaf864831934f8e294b65ad171fa2f83ee0c08e
DVZ <orionvega5@gmail.com>
2022-08-15 20:55:05 +0300

    Add Task 026

---
+++ Task026/Program.cs
@@ -0,0 +1,15 @@
+﻿int LenNumber(int number)
+{
+    int mylen = 0;
+    while (number != 0)
+    {
+        mylen++;
+        number = number / 10;
+    }
+    return mylen;
+}
+void PrintRes(int res)
+{
+    Console.WriteLine(LenNumber(res));
+}
+PrintRes(10000);
---
+++ Task_seminar4/Program.cs
@@ -0,0 +1,10 @@
+﻿int Sum(int a)
+{
+    int result =0;
+    for (int number = 0; number <= a; number++)
+    {
+        result += number;
+    }
+    return result;
+}
+Console.WriteLine(Sum(5));
